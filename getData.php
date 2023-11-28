483165d32dfafa6ab45aa8d54331650ac5b0ec15
Nihat Bayram <nhtem1996@gmail.com>
2023-11-29 02:22:09 +0300

    update

--- getData.php
+++ getData.php
@@ -1,28 +1,57 @@
 <?php
-// getData.php
 
-// API info
-$apiUrl = "https://api.baubuddy.de/dev/index.php/v1/tasks/select";
-$username = "365";
-$password = "1";
+// PERMISSON
 
-// oauth
-$headers = [
-    "Authorization: Basic " . base64_encode("API_Username:API_Password"),
-    "Content-Type: application/json",
-];
-
-// cURL request
 $curl = curl_init();
 curl_setopt_array($curl, [
-    CURLOPT_URL => $apiUrl,
+    CURLOPT_URL => "https://api.baubuddy.de/index.php/login",
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => "{\"username\":\"365\", \"password\":\"1\"}",
+    CURLOPT_HTTPHEADER => [
+        "Authorization: Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
+        "Content-Type: application/json"
+    ],
 ]);
 $response = curl_exec($curl);
+$err = curl_error($curl);
 curl_close($curl);
 
-// return json
-header('Content-Type: application/json');
-echo $response;
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    // auth
+    $access_token = json_decode($response, true)["oauth"]["access_token"];
+
+    // API get data
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_URL => "https://api.baubuddy.de/dev/index.php/v1/tasks/select",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "Authorization: Bearer " . $access_token,
+            "Content-Type: application/json"
+        ],
+    ]);
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        
+        $data = json_decode($response, true);
+    }
+}
+ 
 ?>
